1b1992f4173603692df6e099e6306e601834a095
sekoni90 <sekonioyetunji@gmail.com>
2025-09-25 20:29:16 +0100

    initial commit

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const HomePage: NextPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is a basic Next.js application.</p>
+    </div>
+  );
+};
+
+export default HomePage;
