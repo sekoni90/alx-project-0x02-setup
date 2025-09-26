fa45b7ca325523e5f424efc2355ca7bb3264cd10
sekoni90 <sekonioyetunji@gmail.com>
2025-09-27 00:34:19 +0100

    initial commit

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,20 +1,16 @@
 // pages/home.tsx
 import Head from 'next/head';
+import React from 'react';
 import Header from '../components/layout/Header';
 import Card from '@/components/common/Card';
 
 const HomePage: React.FC = () => {
   return (
     <>
-      <Head>
-        <title>Home Page</title>
-      </Head>
-      <Header />
-      <main className="max-w-6xl mx-auto p-6 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <Card title="Welcome to ALX Project 2" content="This is a reusable card component. you can pass any content here." />
-        <Card title="About this page" content="this page demonstrates how to use the card component with different props." />
-        <Card title="learning Next.js" content=" Next.js pages routing, components, and styling with Tailwind CSS." />
-
+       <Header title="ALX Project" />
+      <main className="p-6 space-y-4">
+        <Card title="First Card" content="This is the content of the first card." />
+        <Card title="Second Card" content="Here’s some more content for the second card." />
       </main>
     </>
   );
